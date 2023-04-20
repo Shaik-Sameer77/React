98aa63f618543d83a8780e90f5351ecb7140fae3
sameer <sameer.shammu.1514@gmail.com>
2023-04-20 13:10:00 +0530

    creating branch1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-// import ReactLogo from './reactlogo'
-// import Button from './buttton';
-// import ReactBenefits from './Benifits'
+
+function Button(){
+  <a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button>Get started...</button>
+</a>
+}
+
+function ReactBenefits(){
+  <div>
+    <h2>Benifits of using React...</h2>
+  <ol>
+     <li>Component-based architecture</li>
+     <li>Virtual DOM for efficient updates</li>
+     <li>Rich ecosystem and community</li>
+     <li>Cross-platform development</li>
+     <li>Strong community support</li>
+ </ol>
+ </div>
+
+}
+
+function ReactLogo(){
+  <img src={logo} className="App-logo" alt="logo" />
+}
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Benifits of using React...</h2>
-          <ol>
-             <li>Component-based architecture</li>
-             <li>Virtual DOM for efficient updates</li>
-             <li>Rich ecosystem and community</li>
-             <li>Cross-platform development</li>
-             <li>Strong community support</li>
-         </ol>
-         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>Get started...</button>
-        </a>
+         <ReactLogo />
+         <ReactBenefits />
+         <Button />
       </header>
     </div>
   );
