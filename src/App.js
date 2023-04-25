aff760b012c74064f9ff6ef5f731fc13dce80417
sameer <sameer.shammu.1514@gmail.com>
2023-04-25 10:14:03 +0530

    done with the Assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
+import React from "react";
 import './App.css';
-import logo from './logo.svg';
+import ProfileCard from "./Profilecard";
 
+const App = () => {
+  const userOne = {
+    imageSrc: "https://s.ndtvimg.com/images/entities/300/virat-kohli-967.png",
+    designation: "Former Captain, Indian Cricket Team",
+    name: "Virat Kohli",
+    jobDescription: "One of the Best batsmen in the world.",
+  };
 
-function App() {
+  const userTwo = {
+    imageSrc: "https://s.ndtvimg.com/images/entities/300/ms-dhoni-700.png",
+    designation: "Former Captain, Indian Cricket Team",
+    name: "MS Dhoni",
+    jobDescription: "Greatest wicket-keeper batsmen.",
+  };
+
+  const userThree = {
+    imageSrc: "https://s.ndtvimg.com/images/entities/300/rohit-sharma-857.png",
+    designation: "Captain, Indian Cricket Team",
+    name: "Rohit Sharma",
+    jobDescription: "one of the Best opening batsmen in the world.",
+  };
+  
+  const handleSeeMore = (name) => {
+    alert(name);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>Get started...</button>
-        </a>
-      </header>
-    </div>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+  <div style={{ backgroundColor: "#5b2c8c", padding: "20px", borderRadius: "10px", marginBottom: "20px" }}>
+    <ProfileCard
+      imageSrc={userOne.imageSrc}
+      designation={userOne.designation}
+      name={userOne.name}
+      jobDescription={userOne.jobDescription}
+      onSeeMore={() => handleSeeMore(userOne.name)}
+    />
+  </div>
+  <div style={{ backgroundColor: "#5b2c8c", padding: "20px", borderRadius: "10px", marginBottom: "20px" }}>
+    <ProfileCard
+      imageSrc={userTwo.imageSrc}
+      designation={userTwo.designation}
+      name={userTwo.name}
+      jobDescription={userTwo.jobDescription}
+      onSeeMore={() => handleSeeMore(userTwo.name)}
+    />
+  </div>
+  <div style={{ backgroundColor: "#5b2c8c", padding: "20px", borderRadius: "10px", marginBottom: "20px" }}>
+    <ProfileCard
+      imageSrc={userThree.imageSrc}
+      designation={userThree.designation}
+      name={userThree.name}
+      jobDescription={userThree.jobDescription}
+      onSeeMore={() => handleSeeMore(userThree.name)}
+    />
+  </div>
+</div>
   );
-}
+};
 
 export default App;
