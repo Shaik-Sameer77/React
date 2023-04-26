fe82dc08c76955e5b39ded6d91eb0dec55a3819d
sameer <sameer.shammu.1514@gmail.com>
2023-04-26 10:42:57 +0530

    done with assignment2 of day5 need to check necessary css

--- src/App.js
+++ src/App.js
@@ -1,71 +1,62 @@
 import './App.css';
 import ProductCard from './component/Productcard';
 
-const Product1 = {
-  productId: '1',
-  productPic: 'https://www.jiomart.com/images/product/original/rvrgwpjvsp/bruton-trendy-sports-shoes-for-men-blue-product-images-rvrgwpjvsp-0-202209021256.jpg',
-  productName: 'Product 1',
-  Brand: 'Brand 1',
-  productDescription: 'This is product 1',
-  Price: '$7',
-  Rating: 4,
-};
-
-const Product2 = {
-  productId: '2',
-  productPic: 'https://5.imimg.com/data5/MT/MB/EO/SELLER-14290347/men-555-black-fashion-lace-up-shoes-500x500.jpg',
-  productName: 'Product 2',
-  Brand: 'Brand 2',
-  productDescription: 'This is product 2',
-  Price: '$5',
-  Rating: 3.5,
-};
-
-const Product3 = {
-  productId: '3',
-  productPic: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/585e2cd2-4f2a-408c-a8ba-f89952cdf332/revolution-6-road-running-shoes-NC0P7k.png',
-  productName: 'Product 3',
-  Brand: 'Brand 3',
-  productDescription: 'This is product 3',
-  Price: '$10',
-  Rating: 4.5,
-};
-
-const Product4 = {
-  productId: '4',
-  productPic: 'https://assets.ajio.com/medias/sys_master/root/20211224/1tuJ/61c4c229aeb26901101a2a6a/-473Wx593H-469034008-black-MODEL.jpg',
-  productName: 'Product 4',
-  Brand: 'Brand 4',
-  productDescription: 'This is product 4',
-  Price: '$25',
-  Rating: 4,
-};
-
-const Product5 = {
-  productId: '5',
-  productPic: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/29abad8d7ae64654b001adb90136819e_9366/RUNMAGICA_SHOES_Blue_EY2972_01_standard.jpg',
-  productName: 'Product 5',
-  Brand: 'Brand 5',
-  productDescription: 'This is product 5',
-  Price: '$9',
-  Rating: 5,
-};
+const productDetails = [
+  {
+    productId: '1',
+    productPic: 'https://www.jiomart.com/images/product/original/rvrgwpjvsp/bruton-trendy-sports-shoes-for-men-blue-product-images-rvrgwpjvsp-0-202209021256.jpg',
+    productName: 'Product 1',
+    brand: 'Brand 1',
+    productDescription: 'This is product 1',
+    price: '$7',
+    rating: 4,
+  },
+  {
+    productId: '2',
+    productPic: 'https://5.imimg.com/data5/MT/MB/EO/SELLER-14290347/men-555-black-fashion-lace-up-shoes-500x500.jpg',
+    productName: 'Product 2',
+    brand: 'Brand 2',
+    productDescription: 'This is product 2',
+    price: '$5',
+    rating: 3.5,
+  },
+  {
+    productId: '3',
+    productPic: 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/585e2cd2-4f2a-408c-a8ba-f89952cdf332/revolution-6-road-running-shoes-NC0P7k.png',
+    productName: 'Product 3',
+    brand: 'Brand 3',
+    productDescription: 'This is product 3',
+    price: '$10',
+    rating: 4.5,
+  },
+  {
+    productId: '4',
+    productPic: 'https://assets.ajio.com/medias/sys_master/root/20211224/1tuJ/61c4c229aeb26901101a2a6a/-473Wx593H-469034008-black-MODEL.jpg',
+    productName: 'Product 4',
+    brand: 'Brand 4',
+    productDescription: 'This is product 4',
+    price: '$25',
+    rating: 4,
+  },
+  {
+    productId: '5',
+    productPic: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/29abad8d7ae64654b001adb90136819e_9366/RUNMAGICA_SHOES_Blue_EY2972_01_standard.jpg',
+    productName: 'Product 5',
+    brand: 'Brand 5',
+    productDescription: 'This is product 5',
+    price: '$9',
+    rating: 5,
+  },
+];
 
 function App() {
   return (
     <div className="product-container">
-      <div className="product-row">
-        <ProductCard productDetails={Product1} />
-        <ProductCard productDetails={Product2} />
-        <ProductCard productDetails={Product3} />
-      </div>
-      <div className="product-row">
-        <ProductCard productDetails={Product4} />
-        <ProductCard productDetails={Product5} />
-      </div>
+      {productDetails.map((productDetail) => (
+        <ProductCard key={productDetail.productId} productDetails={productDetail} />
+      ))}
     </div>
   );
 }
 
 export default App;
-
--- src/component/Productcard.js
+++ src/component/Productcard.js
@@ -1,18 +1,19 @@
 import './productCard.css';
 
-export default function ProductCard({ productDetails, addToCart }) {
+export default function ProductCard({ productDetails}) {
   const {
     productId,
     productPic,
     productName,
-    Brand,
+    brand,
     productDescription,
-    Price,
-    Rating,
+    price,
+    rating,
   } = productDetails;
-
+  
+console.log(productDetails)
   const handleAddToCart = () => {
-    alert(`Product ${productId} - ${productName} of brand ${Brand} with price ${Price} has been added to the cart. Please proceed to pay!`);
+    alert(`Product ${productId} - ${productName} of brand ${brand} with price ${price} has been added to the cart. Please proceed to pay!`);
   };
 
 
@@ -20,10 +21,10 @@ export default function ProductCard({ productDetails, addToCart }) {
     <div className="product-card">
       <img src={productPic} alt="Product" className= "productpic"/>
       <h2>{productName}</h2>
-      <h3>{Brand}</h3>
+      <h3>{brand}</h3>
       <p>{productDescription}</p>
-      <p>{Price}</p>
-      <p>{`Rating: ${Rating}`}</p>
+      <p>{price}</p>
+      <p>{`Rating: ${rating}`}</p>
       <button onClick={handleAddToCart} className="add-to-cart-btn">
         Add to Cart
       </button>
